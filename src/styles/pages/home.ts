b5531b1e1f11ddc0a8fe839eea9ef8008a8faf4e
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-02-23 22:38:08 -0300

    looking for stripe products

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -9,10 +9,10 @@ export const HomeContainer = styled("main", {
   minHeight: 656,
 });
 
-export const Product = styled("a", {
+export const Product = styled("div", {
   background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%)",
   borderRadius: 8,
-  padding: "0.25rem",
+  //padding: "0.25rem",
   cursor: "pointer",
   position: "relative",
   overflow: 'hidden',
