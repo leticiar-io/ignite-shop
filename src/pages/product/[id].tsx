c3adb1d525d0e8890afe431b1d3b8025918aa05a
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-02-25 20:49:48 -0300

    Product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,24 @@
+import { ProductContainer, ImageContainer, ProductDetails } from "../../styles/pages/product";
 import { useRouter } from "next/router";
 
 export default function Product() {
   const { query } = useRouter(); 
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>camiseta x</h1>
+        <span>R$ 19,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quam facilis quisquam dolore id dolores sapiente explicabo, eius odio, sequi deserunt delectus ducimus optio dolorem quidem vitae nulla accusamus natus!</p>
+        
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
    );
  }
  
